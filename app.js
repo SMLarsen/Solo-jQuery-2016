f723a249c73ca45813a92a8a7e7f30016e457122
Steve Larsen <stevelarsen01@gmail.com>
2016-11-07 15:29:31 -0600

    buidling color cubes

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 console.log('hello world');
 
-$(document).ready(function(event) {
-  event.preventDefault();
+$(document).ready(function() {
 
+  var color = '';
+
+  $("button").on("click", function(event) {
+    event.preventDefault();
+
+    color = $(this).data('color');
+    buildDiv(color);
+    addDiv(color);
+  });
+
+  function buildDiv(color) {
+    console.log(color);
+    $(".container").append('<div class="color-cube ' + color + '"></div>');
+  }
+
+  function addDiv(color) {
+    
+  }
 });

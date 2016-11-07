ac63a31d293b42c99f7ef2c71355e8c60089b12f
Steve Larsen <stevelarsen01@gmail.com>
2016-11-07 15:39:00 -0600

    added counters

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ console.log('hello world');
 $(document).ready(function() {
 
   var color = '';
+  var countRed = 0;
+  var countYellow = 0;
+  var countBlue = 0;
+  var countGreen = 0;
 
   $("button").on("click", function(event) {
     event.preventDefault();
@@ -18,6 +22,23 @@ $(document).ready(function() {
   }
 
   function addDiv(color) {
-    
+    switch (color) {
+      case 'red':
+        countRed++;
+        $('#red').text('Total red: ' + countRed);
+        break;
+      case 'green':
+        countGreen++;
+        $('#green').text('Total red: ' + countGreen);
+        break;
+      case 'yellow':
+        countYellow++;
+        $('#yellow').text('Total red: ' + countYellow);
+        break;
+      default:
+        countBlue++;
+        $('#blue').text('Total red: ' + countBlue);
+
+    }
   }
 });
